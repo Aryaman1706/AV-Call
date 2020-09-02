cafd1db621c5bc241b5d96ad3969168f4e327459
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-09-02 20:47:44 +0530

    fix

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-server.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
   console.log("Server running on port 5000");
 });
