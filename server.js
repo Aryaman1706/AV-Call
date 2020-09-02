5936c088670025b405ae6a74ca3e07d15abd3268
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-09-02 21:03:32 +0530

    fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 
